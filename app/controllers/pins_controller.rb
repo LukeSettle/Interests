e0e9ae4a227214b13f6cc0e6568118fa02b27e73
Luke Settle <lukewestonsettle@gmail.com>
2015-08-13 16:00:53 -0500

    remote form that does nothing

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   def index
+    @pin = Pin.new
     @pins = Pin.all
   end
 
@@ -7,13 +8,30 @@ class PinsController < ApplicationController
     @pin = Pin.new
   end
 
+  def show
+    @pin = Pin.find(params[:id])
+  end
+
   def create
-    @pin = Pin.find(pin_params)
-    if @pin.save
-      flash("Nice Pin!")
-    else
-      flash[:warning] = "Your pin was not created"
-      redirect_to pins_path
+    @pin = Pin.new(pin_params)
+    respond_with do |format|
+      format.html do
+        if request.xhr?
+          render :partial => "pin/show", :locals => { :pin => @pin }, :layout => false, :status => :created
+        else
+          redirect_to @pin
+        end
+      end
+    end
+  else
+    respond_with do |format|
+      format.html do
+        if request.xhr?
+          render :json => @pin.errors, :status => :unprocessable_entity
+        else
+          render :action => :new, :status => :unprocessable_entity
+        end
+      end
     end
   end
 
