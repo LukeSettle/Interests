b539189d848c3430563d87310df009379c2f2aab
Luke Settle <lukewestonsettle@gmail.com>
2015-08-16 20:28:43 -0500

    got rid of image tag

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -39,7 +39,6 @@ $(function(){
         $.each(data, function(i, data){
           $url.append('<li><h1><a href='+ data.url + '>'+ data.url +'</a></h1></li>');
           $url.append('<li><p>note: '+data.note+'</p></li>')
-          $url.append('image_tag attachment_url('+ data +', :'+ data.file +', :fill, 300, 300, format: "jpg")')
         })
       },
       error: function(){
