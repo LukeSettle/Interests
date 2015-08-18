95582d266670a5b1023998c9c273acb49bc27cec
Luke Settle <lukewestonsettle@gmail.com>
2015-08-18 15:10:48 -0500

    keeps adding on but kind of works

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -8,8 +8,7 @@ $(function(){
     var appendPicture = function(data) {
       $url.append('<li><img src="'+ data.file+ '", hight="50%" width="50%"></img><h1><a href="http://'+ data.url+ '">'+ data.url+ '</a></h1><p>note: '+ data.note+ '</p></li>');
     }
-
-    var getPins = $.ajax({
+    $.ajax({
       type: 'GET',
       url: 'pins',
       dataType: 'JSON',
@@ -23,16 +22,12 @@ $(function(){
       }
     });
 
-    $(".modal-toggle").click(function(){
-      $('#myModal').modal('toggle');
-      $('#pin_url').val('');
-      $('#pin_note').val('');
-      $('pin_file').val('')
-    });
-
-    $('#create_pin_form').submit(function(){
-      $("#myModal").hide;
-      getPins = $.ajax({
+    $("#create_pin_form").submit(function(){
+      success:
+      $("#myModal").modal('toggle'),
+      console.log("clicks"),
+      $("#url").empty(),
+      $.ajax({
         type: 'GET',
         url: 'pins',
         dataType: 'JSON',
@@ -45,5 +40,27 @@ $(function(){
           alert("Error loading pins")
         }
       });
+      $("#url").empty(),
+      console.log("empty")
+      $.ajax({
+        type: 'GET',
+        url: 'pins',
+        dataType: 'JSON',
+        success: function(data) {
+          $.each(data, function(i, data){
+            appendPicture(data);
+          })
+        },
+        error: function(){
+          alert("Error loading pins")
+        }
+      });
+    });
+
+    $(".modal-toggle").click(function(){
+      $('#myModal').modal('toggle');
+      $('#pin_url').val('');
+      $('#pin_note').val('');
+      $('#pin_file').val('');
     });
   });
